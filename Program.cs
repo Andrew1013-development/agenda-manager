0b158d269fef5da6faf48ca139ecbdeafd65a6da
Andrew1013-development <viethuytran147@gmail.com>
2023-07-07 11:07:43 +0700

    aaaaaaaaaaa

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Bson;
 using csharp_mongodb_quickstart;
-using System.Collections.Specialized;
-using Windows.Management.Update;
 
 string version = "1.0.0";
 string? connectionString = "mongodb+srv://homework-database:homework123@cluster0.ygoqv7l.mongodb.net/";
+string uploader_password = DateTime.Now.ToShortDateString();
 if (connectionString == string.Empty) {
-    Console.WriteLine("no connection string specified in source code");   
+    Console.WriteLine("Cannot establish connection to database.");   
     Environment.Exit(1);
 }
 
@@ -25,11 +24,16 @@ Console.WriteLine();
 
 // specify role (uploader / receiver)
 string? role = "";
-Console.WriteLine("What is your role? 1 for uploader, 2 for receiver, 3 for exiting");
+Console.WriteLine("Role selection:");
+Console.WriteLine("1: Uploader (uploading agendas to database)\n" +
+    "2: Receiver (receiving upcoming agendas from database)\n" +
+    "3: Pruner (pruning old out-of-date agendas from database)\n" + 
+    "4: Exiting (exit the program)");
+Console.WriteLine("Specifiy your role (1 for uploader, 2 for receiver, 3 for pruner, 4 for exiting) ");
 while (role == string.Empty || role == null) {
     Console.Write("Role: ");
     role = Console.ReadLine();
-    if (role == "1" || role == "2" || role == "3") {
+    if (role == "1" || role == "2" || role == "3" || role == "4") {
         break;
     }
     else {
@@ -39,42 +43,62 @@ while (role == string.Empty || role == null) {
 
 if (role == "1")
 {
-    // user input
-    Console.Write("Subject: ");
-    string? subject_input = Console.ReadLine();
-    Console.Write("Deadline: ");
-    string? deadline_input = DateTime.Parse(Console.ReadLine()).ToShortDateString();
-    Console.Write("Content: ");
-    string? content_input = Console.ReadLine();
-    Console.Write("Notes: ");
-    string? notes_input = Console.ReadLine();
-
-    Console.WriteLine("Uploading to database.....");
-    // turn into "data packet"
-    Agenda newAgenda = new()
+    Console.Write("Specify your password: ");
+    string? output = Console.ReadLine();
+    if (output == uploader_password)
     {
-        subject = subject_input,
-        deadline = deadline_input,
-        content = content_input,
-        notes = notes_input,
-        created = DateTime.Now.ToShortDateString(),
-    };
-    Console.WriteLine(newAgenda.ToBsonDocument());
-    // insert data packet into database
-    agenda_collection.InsertOne(newAgenda);
-    Console.WriteLine("Uploaded to database.");
+        // user input
+        Console.Write("Subject: ");
+        string? subject_input = Console.ReadLine();
+        Console.Write("Deadline: ");
+        string? deadline_input = DateTime.Parse(Console.ReadLine()).ToShortDateString();
+        Console.Write("Content: ");
+        string? content_input = Console.ReadLine();
+        Console.Write("Notes: ");
+        string? notes_input = Console.ReadLine();
+
+        Console.WriteLine("Uploading to database.....");
+        // turn into "data packet"
+        Agenda newAgenda = new()
+        {
+            subject = subject_input,
+            deadline = deadline_input,
+            content = content_input,
+            notes = notes_input,
+            created = DateTime.Now.ToShortDateString(),
+        };
+        Console.WriteLine(newAgenda.ToBsonDocument());
+        // insert data packet into database
+        agenda_collection.InsertOne(newAgenda);
+        Console.WriteLine("Uploaded to database.");
+    }
+    else {
+        Console.WriteLine("Wrong password entered.");
+    }
 }
 else if (role == "2")
 {
+    Console.WriteLine("Fetching agenda database.....");
     // find from filter (greater than the current date on system)
     var agenda_filter = Builders<Agenda>.Filter.Gte("deadline", DateTime.Now.ToShortDateString());
     List<Agenda> agenda_search = agenda_collection.Find(agenda_filter).ToList();
     // list individual agendas searched
-    Console.WriteLine($"Agendas available: {agenda_search.Count()} agendas");
+    Console.WriteLine($"Agendas available: {agenda_search.Count} agendas");
     for (int i = 0; i < agenda_search.Count; i++)
     {
         Console.WriteLine(agenda_search.ElementAt(i).ToBsonDocument());
     }
+}
+else if (role == "3")
+{
+    // find from filter (less than the current date on system)
+    var agenda_filter = Builders<Agenda>.Filter.Lte("deadline", DateTime.Now.ToShortDateString());
+    List<Agenda> agenda_search = agenda_collection.Find(agenda_filter).ToList();
+    Console.WriteLine($"Agendas to be deleted: {agenda_search.Count}");
+    Console.WriteLine($"Pruning {agenda_search.Count} old agendas.....");
+    // delete individual agendas
+    agenda_collection.DeleteMany(agenda_filter);
+    Console.WriteLine($"Pruned {agenda_search.Count} old agendas");
 }
 else {
     Console.WriteLine("Exiting.....");
